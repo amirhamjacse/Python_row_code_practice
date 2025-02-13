1212ba90b5e04bbdb5f3e354d074e89731d72451
amirhamjacse <amirhamza824@gmail.com>
2025-02-13 12:06:13 +0600

    learn about logging

--- logger.py
+++ logger.py
@@ -1,12 +1,16 @@
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    filename='app.log', level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s' ,
+    # filemode='w'
+    )
 
 # Logging messages
 logging.debug("This is a debug message")
 logging.info("This is an info message")
-logging.warning("This is a warning message")
+logging.warning("This is a warning message1")
 logging.error("This is an error message")
 
 logging.critical("This is a critical message")
