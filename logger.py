d7d632a80c17e6adcd8441847b95b75095d7d32e
amirhamjacse <amirhamza824@gmail.com>
2025-02-13 09:15:53 +0600

    updated

--- logger.py
+++ logger.py
@@ -8,4 +8,5 @@ logging.debug("This is a debug message")
 logging.info("This is an info message")
 logging.warning("This is a warning message")
 logging.error("This is an error message")
+
 logging.critical("This is a critical message")
