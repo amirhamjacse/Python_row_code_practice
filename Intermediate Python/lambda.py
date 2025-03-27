9a3e6e51efaf6fea592be60246701d4769980dc6
amirhamjacse <amirhamza824@gmail.com>
2025-03-27 16:02:18 +0600

    added lambda function

---
+++ Intermediate Python/lambda.py
@@ -0,0 +1,18 @@
+squre = lambda x: x*x
+
+print(squre(2))
+"""
+    Gap
+"""
+sumofnum = lambda a,b: a+b
+
+print(sumofnum(2,3))
+
+"""
+    Gap
+"""
+number = [1,2,3,4,5]
+
+squred = list(map(lambda x:x**x, number))
+
+print(squred)
