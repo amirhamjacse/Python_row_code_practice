74351a537cd63b068b1db50a8fea4f86803c3e99
amirhamjacse <amirhamza824@gmail.com>
2025-03-27 15:55:39 +0600

    updated

--- Intermediate Python/args_kwargs.py
+++ Intermediate Python/args_kwargs.py
@@ -12,3 +12,11 @@ def show_parameter(*args, **kwargs):
         print(f"{key}: {value}")
 
 show_parameter(1,2,3, name="amir")
+
+
+def show_details(name, *args, **kwargs):
+    print(f"Name: {name}")
+    print(f"Other Info: {args}")  # Tuple
+    print(f"Additional Info: {kwargs}")  # Dictionary
+
+show_details("Amir", "Python Developer", 25, city="Dhaka", country="Bangladesh")
