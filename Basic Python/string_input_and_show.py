4ad0ecb661683497b1ef0ff0d6dd09d71c209fa6
amirhamjacse <amirhamza824@gmail.com>
2025-11-16 12:16:42 +0600

    practicing

--- Basic Python/string_input_and_show.py
+++ Basic Python/string_input_and_show.py
@@ -1,2 +1,7 @@
 name = input("Input your name: \n")
 print(name)
+
+
+name = input("Input your name again: \n")
+var_id = input("Input a your id number: \n")
+print("Your name is " + name + " and your id is " + var_id+" Thansks!")
