18066c024bd09d30d855deafa9c62e5958fe8df1
amirhamjacse <amirhamza824@gmail.com>
2026-02-16 11:18:50 +0600

    updated no list numpy

--- Numpy/list.py
+++ Numpy/list.py
@@ -25,3 +25,17 @@ b = np.array([4, 5, 6])
 
 print(a + b)   # [5 7 9]
 print(a * b)   # [4 10 18]
+
+a1 = np.zeros((2,3))   # matrix full of 0
+a2 = np.ones((2,3))    # matrix full of 1
+a3 = np.arange(0,10000)   # like range()
+a4 = np.linspace(0,1,5) # 5 numbers between 0 and 1
+
+print(a1,a2,a3,a4)
+
+
+b1 = np.random.rand(2,2)  # random numbers 0-1
+b2 = np.random.randint(1,10,5)  # random integers
+print(b1, b2)
+
+
