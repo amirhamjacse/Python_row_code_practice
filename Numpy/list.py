7d1555333891080b773932a66b3cd77f2c47bf1e
amirhamjacse <amirhamza824@gmail.com>
2026-02-16 11:11:34 +0600

    numpy math operation

--- Numpy/list.py
+++ Numpy/list.py
@@ -1,5 +1,5 @@
 import numpy as np
-a = [1, 3, 2]
+ab = [1, 3, 2]
 
 # 1D array using numpy
 a = np.array([1, 2, 3])
@@ -16,3 +16,12 @@ print(a.ndim)    # number of dimensions
 print(arr.ndim)    # number of dimensions
 print(a.size)    # total elements
 print(a.dtype)   # data type
+
+print(a+arr, "array sum") #array addition
+
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(a + b)   # [5 7 9]
+print(a * b)   # [4 10 18]
